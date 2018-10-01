29fd1f7081e3a72e3a994d695ab75407d6c508d4
DavidMatovic <Matovic1314@students.d211.org>
2018-10-01 14:09:56 -0500

    commitOmitTest

--- app/src/main/java/com/example/rc211/volleyballstattracker/MainActivity.java
+++ app/src/main/java/com/example/rc211/volleyballstattracker/MainActivity.java
@@ -4,8 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//test comment
-    //test comment 2
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
