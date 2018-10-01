f24a9a9038be74df4dd5a3cbaa66098d1a01adfa
DavidMatovic <Matovic1314@students.d211.org>
2018-10-01 14:06:31 -0500

    commit2

--- app/src/main/java/com/example/rc211/volleyballstattracker/MainActivity.java
+++ app/src/main/java/com/example/rc211/volleyballstattracker/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //test comment
+    //test comment 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
